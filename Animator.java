851823acf45f14aa4288da2faf2d4c036662870e
Huml4n <h.forsling@gmail.com>
2024-09-24 10:09:54 +0200

    fixed

--- Animator.java
+++ Animator.java
@@ -12,7 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import bouncing_balls.Model.Ball;
+import ballz.Model.Ball;
 
 /**
  * Animated JPanel drawing the bouncing balls. No modifications are needed in this class.
