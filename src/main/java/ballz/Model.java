05ea47fa37f7df7a836e94d8b554a7e498536f7b
simon-wg <simon.green@live.se>
2024-09-24 18:16:54 +0200

    Trying to add mass

--- src/main/java/ballz/Model.java
+++ src/main/java/ballz/Model.java
@@ -2,161 +2,171 @@ package ballz;
 
 import java.lang.Math;
 import java.util.HashMap;
-import java.util.ArrayList;
 
 /**
  * The physics model.
- * 
+ *
  * This class is where you should implement your bouncing balls model.
- * 
- * The code has intentionally been kept as simple as possible, but if you wish, you can improve the design.
- * 
+ *
+ * The code has intentionally been kept as simple as possible, but if you wish,
+ * you can improve the design.
+ *
  * @author Simon Robillard
  *
  */
 class Model {
 
-	double areaWidth, areaHeight;
+    double areaWidth, areaHeight;
 
-	final double GRAVITY = -0.982;
+    final double GRAVITY = -0;
 
-	double EXPECTED_ENERGY;
-	
-	double energy;
-	
-	Ball[] balls;
+    double energy;
 
-	Model(double width, double height) {
-		areaWidth = width;
-		areaHeight = height;
+    Ball[] balls;
 
-		
-		// Initialize the model with a few balls
-		balls = new Ball[3];
-		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
-		balls[2] = new Ball(width / 3, height * 0.4, -1, -1.0, 0.5);
+    Model(double width, double height) {
+        areaWidth = width;
+        areaHeight = height;
 
-		EXPECTED_ENERGY = 0.5 * (balls[0].vx * balls[0].vx + balls[0].vy * balls[0].vy +
-				balls[1].vx * balls[1].vx + balls[1].vy * balls[1].vy +
-				balls[2].vx * balls[2].vx + balls[2].vy * balls[2].vy) - GRAVITY * (balls[0].y + balls[1].y + balls[2].y);
-	}
+        // Initialize the model with a few balls
+        balls = new Ball[3];
+        balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2, 1);
+        balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3, 1);
+        balls[2] = new Ball(width / 3, height * 0.4, -1, -1.0, 0.5, 10);
+    }
 
-	void step(double deltaT) {
-		// TODO this method implements one step of simulation with a step deltaT
-		energy = 0;
-		HashMap<Ball, Ball> collisions = new HashMap<>();
-		for (Ball b : balls) {
-			// detect collision with the border
-			if (b.x < b.radius || b.x > areaWidth - b.radius) {
-				b.vx *= -1; // change direction of ball
-				b.x = Math.clamp(b.x, b.radius, areaWidth - b.radius); // make sure the ball is within the area
-			}
-			if (b.y < b.radius || b.y > areaHeight - b.radius) {
-				b.vy *= -1;
-				b.y = Math.clamp(b.y, b.radius, areaHeight - b.radius);
-			}
+    void step(double deltaT) {
+        // TODO this method implements one step of simulation with a step deltaT
+        energy = 0;
+        HashMap<Ball, Ball> collisions = new HashMap<>();
+        for (Ball b : balls) {
+            // detect collision with the border
+            if (b.x < b.radius || b.x > areaWidth - b.radius) {
+                b.vx *= -1; // change direction of ball
+                b.x = Math.clamp(b.x, b.radius, areaWidth - b.radius); // make sure the ball is within the area
+            }
+            if (b.y < b.radius || b.y > areaHeight - b.radius) {
+                b.vy *= -1;
+                b.y = Math.clamp(b.y, b.radius, areaHeight - b.radius);
+            }
 
-			// detect collision with other balls
-			for (Ball other : balls) {
-				if (b == other || (collisions.containsKey(b) && collisions.get(b) == other)) {
-					continue;
-				}
-				collisions.put(b, other);
-				double distance = b.distanceToOtherBall(other);
+            // detect collision with other balls
+            for (Ball other : balls) {
+                if (b == other || (collisions.containsKey(b) && collisions.get(b) == other)) {
+                    continue;
+                }
+                collisions.put(b, other);
 
-				if (distance <=0) {
-					double angle = b.angleToOtherBall(other);
+                if (b.distanceToOtherBall(other) <= 0) {
+                    // rotate the speed of the balls
+                    collide(b, other);
+                }
+            }
 
-					// rotate the speed of the balls
-					double[] b_rotated_v = rotate(b.vx, b.vy, -angle);
-					double[] other_rotated_v = rotate(other.vx, other.vy, -angle);
+            b.vy += deltaT * GRAVITY;
 
-					double[] b_unrotated_v = rotateInverse(other_rotated_v[0], b_rotated_v[1], -angle);
-					double[] other_unrotated_v = rotateInverse(b_rotated_v[0], other_rotated_v[1], -angle);
+            // compute new position according to the speed of the ball
+            b.x += deltaT * b.vx;
+            b.y += deltaT * b.vy;
 
-					b.vx = b_unrotated_v[0];
-					b.vy = b_unrotated_v[1];
+            energy += b.mass * Math.pow(b.getVelocity(), 2) / 2 - b.mass * GRAVITY * b.y;
+        }
 
-					other.vx = other_unrotated_v[0];
-					other.vy = other_unrotated_v[1];
+        System.out.println(energy);
 
-					// move the balls so they don't overlap
-				}
-			}
+        // Adjust for energy loss by floating point inaccuracy
+        // double energy_factor = Math.sqrt(EXPECTED_ENERGY / energy);
+        // for (Ball b : balls) {
+        // b.vx *= energy_factor;
+        // b.vy *= energy_factor;
+        // }
+    }
 
+    void collide(Ball b, Ball other) {
+        double distance = b.distanceToOtherBall(other);
 
-			b.vy += deltaT * GRAVITY;
-			
-			// compute new position according to the speed of the ball
-			b.x += deltaT * b.vx;
-			b.y += deltaT * b.vy;
-		
-			energy += (b.vx * b.vx + b.vy * b.vy)/2 - GRAVITY * b.y;
-		}
+        double angle = b.angleToOtherBall(other);
+        double[] b_rotated = rotate(b.vx, b.vy, -angle);
+        double[] other_rotated = rotate(other.vx, other.vy, -angle);
 
-		System.out.println(energy);
+        double b_vx = (b.mass - other.mass) / (b.mass + other.mass) * b_rotated[0]
+                + 2 * other.mass / (b.mass + other.mass) * other_rotated[0];
 
-		// Adjust for energy loss by floating point inaccuracy
-		// double energy_factor = Math.sqrt(EXPECTED_ENERGY / energy);
-		// for (Ball b : balls) {
-		// 	b.vx *= energy_factor;
-		// 	b.vy *= energy_factor;
-		// }
-	}
+        double other_vx = 2 * b.mass / (b.mass + other.mass) * b_rotated[0];
 
-	double[] rectToPolar(double x, double y) {
-		double r = Math.sqrt(x * x + y * y);
-		double theta = Math.atan2(y, x);
-		return new double[]{r, theta};
-	}
+        double[] b_unrotated = rotateInverse(b_vx, b_rotated[1], -angle);
+        double[] other_unrotated = rotateInverse(other_vx, other_rotated[1], -angle);
 
-	double[] polarToRect(double r, double theta) {
-		double x = r * Math.cos(theta);
-		double y = r * Math.sin(theta);
-		return new double[]{x, y};
-	}
+        b.vx = b_unrotated[0];
+        b.vy = b_unrotated[1];
 
-	double[] rotate(double x, double y, double angle) {
-		double new_x = x * Math.cos(angle) - y * Math.sin(angle);
-		double new_y = x * Math.sin(angle) + y * Math.cos(angle);
-		return new double[]{new_x, new_y};
-	}
+        other.vx = other_unrotated[0];
+        other.vy = other_unrotated[1];
 
-	double[] rotateInverse(double x, double y, double angle) {
-		double new_x = x * Math.cos(angle) + y * Math.sin(angle);
-		double new_y = -x * Math.sin(angle) + y * Math.cos(angle);
-		return new double[]{new_x, new_y};
-	}
-	
-	/**
-	 * Simple inner class describing balls.
-	 */
-	class Ball {
-		
-		Ball(double x, double y, double vx, double vy, double r) {
-			this.x = x;
-			this.y = y;
-			this.vx = vx;
-			this.vy = vy;
-			this.radius = r;
-		}
+        b.x += distance / 2 * Math.cos(angle);
+        b.y += distance / 2 * Math.sin(angle);
 
-		/**
-		 * Position, speed, and radius of the ball. You may wish to add other attributes.
-		 */
-		double x, y, vx, vy, radius;
+        other.x -= distance / 2 * Math.cos(angle);
+        other.y -= distance / 2 * Math.sin(angle);
 
-		double angleToOtherBall(Ball other) {
-			double dx = other.x - x;
-			double dy = other.y - y;
-			return Math.atan2(dy, dx);
-		}
+    }
 
-		double distanceToOtherBall(Ball other) {
-			double dx = other.x - x;
-			double dy = other.y - y;
-			return Math.sqrt(dx * dx + dy * dy) - radius - other.radius;
-		}
-	}
+    double[] rectToPolar(double x, double y) {
+        double r = Math.sqrt(x * x + y * y);
+        double theta = Math.atan2(y, x);
+        return new double[] { r, theta };
+    }
+
+    double[] polarToRect(double r, double theta) {
+        double x = r * Math.cos(theta);
+        double y = r * Math.sin(theta);
+        return new double[] { x, y };
+    }
+
+    double[] rotate(double x, double y, double angle) {
+        double new_x = x * Math.cos(angle) - y * Math.sin(angle);
+        double new_y = x * Math.sin(angle) + y * Math.cos(angle);
+        return new double[] { new_x, new_y };
+    }
+
+    double[] rotateInverse(double x, double y, double angle) {
+        return rotate(x, y, -angle);
+    }
+
+    /**
+     * Simple inner class describing balls.
+     */
+    class Ball {
+
+        Ball(double x, double y, double vx, double vy, double r, double m) {
+            this.x = x;
+            this.y = y;
+            this.vx = vx;
+            this.vy = vy;
+            this.radius = r;
+            this.mass = m;
+        }
+
+        /**
+         * Position, speed, and radius of the ball. You may wish to add other
+         * attributes.
+         */
+        double x, y, vx, vy, radius, mass;
+
+        double angleToOtherBall(Ball other) {
+            double dx = other.x - x;
+            double dy = other.y - y;
+            return Math.atan2(dy, dx);
+        }
+
+        double distanceToOtherBall(Ball other) {
+            double dx = other.x - x;
+            double dy = other.y - y;
+            return Math.sqrt(dx * dx + dy * dy) - radius - other.radius;
+        }
+
+        double getVelocity() {
+            return Math.sqrt(vx * vx + vy * vy);
+        }
+    }
 }
